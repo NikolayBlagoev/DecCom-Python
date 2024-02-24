aea2639e081b338bf710feb61e5cc7e112f7977f
nblagoev <n.blagoev@student.tudelft.nl>
2024-02-25 00:42:28 +0200

    small changes for testing

--- 2.py
+++ 2.py
@@ -7,8 +7,8 @@ from deccom.protocols.streamprotocol import StreamProtocol
 from random import randint
 
 n = Peer(("127.0.0.1", 10015))
-# def send(nd: StreamNode):
-#     asyncio.ensure_future(nd.find_node("3"))
+def send(nd: StreamNode):
+    asyncio.ensure_future(nd.find_node("3"))
 protocol = DefaultProtocol()
 gossip = GossipDiscovery(bootstrap_peers=[n])
 gossip.set_lower(protocol)
@@ -18,9 +18,9 @@ me = StreamNode(stream,"127.0.0.1")
 Peer.me = Peer((me.ip_addr,me.port), tcp=me.tcp_port, pub_key="2")
 loop = asyncio.new_event_loop()
 print(Peer.me.id_node)
-# if Peer.me.pub_key == "2":
-#     loop.call_later(5,
-#                                         send, me)
+if Peer.me.pub_key == "2":
+    loop.call_later(5,
+                                        send, me)
 loop.run_until_complete(me.listen())
 
 loop.run_forever()
--- 3.py
+++ 3.py
@@ -6,19 +6,21 @@ from deccom.peers import Peer
 from deccom.protocols.streamprotocol import StreamProtocol
 from random import randint
 
-
 n = Peer(("127.0.0.1", 10015))
 # def send(nd: StreamNode):
 #     asyncio.ensure_future(nd.find_node("3"))
 protocol = DefaultProtocol()
 gossip = GossipDiscovery(bootstrap_peers=[n])
 gossip.set_lower(protocol)
-stream = StreamProtocol(True, peer_connected_callback = print, disconnected_callback=print)
+stream = StreamProtocol(True, peer_connected_callback= print, disconnected_callback=print)
 stream.set_lower(gossip)
 me = StreamNode(stream,"127.0.0.1")
 Peer.me = Peer((me.ip_addr,me.port), tcp=me.tcp_port, pub_key="3")
 loop = asyncio.new_event_loop()
 print(Peer.me.id_node)
+# if Peer.me.pub_key == "2":
+#     loop.call_later(5,
+#                                         send, me)
 loop.run_until_complete(me.listen())
 
 loop.run_forever()
