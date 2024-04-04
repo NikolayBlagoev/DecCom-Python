feb8275c12470d86581925225e8040bc5c7edfdf
nblagoev <n.blagoev@student.tudelft.nl>
2024-04-04 15:44:37 +0300

    minor fixes

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import setup, find_packages
 
 with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
-with open("requirements.txt", encoding="utf-8") as f:
-    requirements = f.readlines()
+
 setup(
     name="deccom",
     version="0.0.1",
@@ -14,17 +13,17 @@ setup(
     author_email="nickyblagoev@gmail.com",
     license="MIT",
     url="https://github.com/NikolayBlagoev/DecCom-Python",
-    packages=find_packages(),
-    install_requires=requirements,
+    packages=["deccom"],
+    install_requires=[
+        'cryptography',
+        'fe25519',
+        'ge25519'
+    ],
     classifiers=[
       "Intended Audience :: Developers",
       "License :: OSI Approved :: MIT License",
       "Operating System :: OS Independent",
       "Programming Language :: Python",
-      "Programming Language :: Python :: 3",
-      "Programming Language :: Python :: 3.5",
-      "Programming Language :: Python :: 3.6",
-      "Programming Language :: Python :: 3.7",
-      "Topic :: Software Development :: Libraries :: Python Modules",
+      "Programming Language :: Python :: 3"
     ]
 )
